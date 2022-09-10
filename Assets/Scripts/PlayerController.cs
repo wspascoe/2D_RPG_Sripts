788b46c66265e4252e7a4ea2b7118b3dd11f4d33
Bill <wspacoe@gmail.com>
2022-09-10 06:18:44 -0400

    Player Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-
+    [SerializeField] SpriteRenderer playerSprite;
     private Controls controls;
     private Rigidbody2D rBody;
+    private Animator animator;
     private Vector2 moveInput;
+    private bool isMoving = false;
 
     private void Awake()
     {
         controls = new Controls();
         rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -26,9 +30,39 @@ public class PlayerController : MonoBehaviour
         controls.Player.Disable();
     }
 
+    private void Update()
+    {
+        UpdateAnimator();
+    }  
+
     private void FixedUpdate()
     {
         moveInput = controls.Player.Movement.ReadValue<Vector2>();
         rBody.velocity = moveInput * speed;
+    }
+
+    private void UpdateAnimator()
+    {
+        if(moveInput == Vector2.zero)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+        //Set Bool based on whether we are moving or not
+        animator.SetBool("isWalking", isMoving);
+
+        //We use this because we dont have 4 dir sprites
+        if (moveInput.x < 0 && isMoving) //Going Left
+        {
+            playerSprite.flipX = true;
+        }
+        else if (moveInput.x > 0 && isMoving)
+        {
+            playerSprite.flipX = false;
+        }
+
     }
 }
